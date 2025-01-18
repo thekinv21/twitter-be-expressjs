e3ceef07967272b6592c9b33c4cb9373467f57d6
Vadim Kiniabaev <thekinv21@gmail.com>
2025-01-18 03:08:34 +0300

    feat: export HTTP enums alongside API response types for enhanced response handling

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,3 +1,16 @@
+import {
+	HttpCodeEnum,
+	HttpMessageEnum,
+	HttpMethodEnum,
+	HttpStatusEnum
+} from './enum/http.enum'
 import { IApiResponse, IPaginatedApiResponse } from './types/custom.types'
 
-export { IApiResponse, IPaginatedApiResponse }
+export {
+	HttpCodeEnum,
+	HttpMessageEnum,
+	HttpMethodEnum,
+	HttpStatusEnum,
+	IApiResponse,
+	IPaginatedApiResponse
+}
